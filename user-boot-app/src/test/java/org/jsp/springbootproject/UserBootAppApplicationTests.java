bcdfb19b8e3524e81b65be2ddc53f4e3c4ae76d3
Basavaraj-me <basavarj7065@gmail.com>
2024-01-27 11:54:28 +0530

    Full Project of User and Product Spring boot

---
+++ user-boot-app/src/test/java/org/jsp/springbootproject/UserBootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jsp.springbootproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserBootAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
